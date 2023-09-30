410e916cdd7106d419cd30697b7733981e3759cd
Incheol-Jung <bluesky60960@gmail.com>
2023-09-30 19:38:29 +0900

    5. 변수 선언

---
+++ src/main/kotlin/examples/Five.kt
@@ -0,0 +1,15 @@
+package examples
+
+/**
+ *
+ * @author Incheol.Jung
+ * @since 2023. 09. 30.
+ */
+fun main() {
+    var a = "처음값"
+    println(a)
+
+    val b : Int = 1
+    // b = 2 ERROR
+    val c = 3
+}
