0b008059083faa30a56f3eaf3eead20bf165e3af
Incheol-Jung <bluesky60960@gmail.com>
2023-10-02 16:51:50 +0900

    34. 문자열 템플릿

---
+++ src/main/kotlin/examples/34.kt
@@ -0,0 +1,13 @@
+package examples
+
+/**
+ *
+ * @author Incheol.Jung
+ * @since 2023. 10. 02.
+ */
+fun main() {
+    val greeting = "Kotliner"
+
+    println("Hello $greeting")                  // 1
+    println("Hello ${greeting.uppercase()}")    // 2
+}
